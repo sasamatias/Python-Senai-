cc6f36429d05a3d3effb18add2dd723fc47a5bfa
sasamatias <sabrinamatias219@gmail.com>
2024-12-05 11:26:16 -0300

    Variavél

---
+++ Aula 02 05.12/aula02.py
@@ -0,0 +1,9 @@
+print('ola mundo')
+nome_usuario='sasamatias'# tipo -> str = string
+idade_usuario=20 # tipo -> int = integer
+altura_usuario= 1.51 # tipo -> float = flutuante
+maior_idade= True # tipo -> bool = boolean
+print('seja bem-vindo', nome_usuario)
+print('sua idade é', idade_usuario,'anos')
+print('você tem', altura_usuario, 'metros.')
+print('você é maior de idade?', maior_idade)
